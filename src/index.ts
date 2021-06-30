2b2ab8062504b2e5002067f7aa187451783d5c9b
Sam <qureshisamin25@gmail.com>
2021-06-30 01:20:34 -0400

    stuff

--- src/index.ts
+++ src/index.ts
@@ -48,12 +48,13 @@ client.on("message", async message => {
         // args = message.content.startsWith(process.env.prefix!)
         //     ? message.content.slice(process.env.prefix!.length).trim().split(/ +/g)
         //     : message.content.slice((`<@!${client.user!.id}>`.length)).trim().split(/ +/g);
-        if(message.content.startsWith(`<@!${client.user!.id}>`)){
-            message.mentions.users.delete(client.user!.id)
-        }
         args = message.content.startsWith(process.env.prefix!)
             ? message.content.replace(process.env.prefix!, "").trim().split(/ +/g)
             : message.content.replace((`<@!${client.user!.id}>`), "").trim().split(/ +/g);
+        
+        if(message.content.startsWith(`<@!${client.user!.id}>`)){
+            message.mentions.users.delete(client.user!.id)
+        }
     }
     else {
         return;
