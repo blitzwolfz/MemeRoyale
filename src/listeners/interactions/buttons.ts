18c82be76c41ee5dcd14f35e6ddd4195fec2c8cb
Sam <qureshisamin25@gmail.com>
2023-08-25 10:16:09 -0400

    v3 Start
    Start of v3 changes

--- src/listeners/interactions/buttons.ts
+++
@@ -1,9 +0,0 @@
-import type { ButtonInteraction } from "discord.js";
-
-export async function interactionButtonsCommand(interaction: ButtonInteraction) {
-    // console.log(interaction);
-    await interaction.reply({
-        ephemeral:false,
-        content:`Button ID: ${interaction.customId} User: <@${interaction.user.id}> Channel: <#${(await interaction.channel!.fetch()).id}>`
-    })
-}
--- src/listeners/interactions/menus.ts
+++
@@ -1,6 +0,0 @@
-import { client } from "../index";
-
-client.on('interactionCreate', interaction => {
-    if (!interaction.isSelectMenu()) return;
-    console.log(interaction);
-});
